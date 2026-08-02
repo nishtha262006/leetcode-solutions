0c95f42c3041d25614abca9406059da361a94953
Nishtha262006 <nishthawadaskar.56@gmail.com>
2026-08-02 16:32:10 +0530

    Added LeetCode 877

---
+++ Arrays/877_Stone_Game.cpp
@@ -0,0 +1,20 @@
+// Observation:
+
+// The number of piles is always even.
+// The total number of stones is always odd, so a tie is impossible.
+// Alice can decide before the game whether to take all even-indexed piles or all odd-indexed piles.
+// She chooses the set with the larger total number of stones.
+// By making the correct first move, Alice can always force herself to keep taking piles from that set.
+// Therefore, Alice is guaranteed to win regardless of Bob's moves.
+
+// Time Complexity: O(1)
+// Space Complexity: O(1)
+
+
+class Solution {
+public:
+    bool stoneGame(vector<int>& piles) {
+        return true;
+    
+    }
+};
